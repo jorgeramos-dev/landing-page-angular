b1b389094c00a1580e9bcc9e677273f0b620fcca
Jorge Ramos <142628950+jorgeramos-dev@users.noreply.github.com>
2024-03-07 21:11:53 -0300

    create components page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AboutComponent,
+    ClientsComponent,
+    FooterComponent,
+    GalleryComponent,
+    HeaderComponent,
+    NavComponent,
+    PricingComponent,
+    ServicesComponent,
+    TestimonialComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
